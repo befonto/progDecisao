997ff233c3fb4e8b8b574eac4adc3decc5d00358
Beatriz Fontoura <befonto@gamil>
2023-08-15 11:54:27 -0300

    Questões lista 041 finalizadas

--- lista041/questao13.py
+++ lista041/questao13.py
@@ -2,10 +2,37 @@
 Desenvolver um programa que pergunte 3 valores (variáveis a, b e c) e ao final apresente-os dispostos em ordem crescente.
 '''
 
-a = int(input("Insira um número inteiro: "))
-b = int(input("Insira outro número inteiro: "))
-c = int(input("Insira um último número inteiro: "))
+a = int(input("Insira um número inteiro para a: "))
+b = int(input("Insira um número inteiro para b: "))
+c = int(input("Insira um número inteiro para c: "))
 
+# 1 - a tem que ser menor que b
+if ( a > b ):
+    aux = a
+    a = b
+    b = aux
+
+# garantido até aqui entre a e b, o menor está em a
+
+# 2 - a tem que ser menor que c
+if (a > c):
+    aux = a
+    a = c
+    c = aux
+
+# garantido até aqui que a é o menor dos 3
+# agora é necessário garantir que b seja menor que c
+
+if ( b > c ):
+    aux = b
+    b = c
+    c = aux
+
+# garantido até aqui que entre b e c, o b é menor, ou seja, o c é maior de todos
+
+print(f"Ordem crescente: {a}, {b} e {c}")
+
+'''
 if a <= b and a <= c and b <= c:
     print(f"A ordem crescente é {a}, {b} e {c}")
 elif a <= b and a <= c and c <= b:
@@ -18,3 +45,4 @@ elif c <= a and c <= b and a <= b:
     print(f"A ordem crescente é {c}, {a} e {b}")
 elif c <= a and c <= b and b <= a:
     print(f"A ordem crescente é {c}, {b} e {a}")
+'''
