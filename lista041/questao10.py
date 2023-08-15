d874aac423f9ac8c33ede8d099ff19bd55a25cf2
Beatriz Fontoura <befonto@gamil>
2023-08-15 10:37:23 -0300

    Primeiro commmit

--- lista041/questao10.py
+++ lista041/questao10.py
@@ -5,7 +5,7 @@ Desenvolver um programa que pergunte dois números inteiros, e apresente como re
 num1 = int(input("Insira um número inteiro: "))
 num2 = int(input("Insira outro número inteiro: "))
 
-a = num1 % num2
+a = num2 % num1
 
 if ( a == 0 ):
     print(f"O número {num1} é divisível por {num2}!")
